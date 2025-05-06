54e4e7762bdfc267008835c1e0030fcf4a003143
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2025-05-06 10:47:02 +0300

    first commit

---
+++ src/main/java/ua/org/kse/bettergist/BetterGistApplication.java
@@ -0,0 +1,13 @@
+package ua.org.kse.bettergist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterGistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterGistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/org/kse/bettergist/BetterGistApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.org.kse.bettergist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterGistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

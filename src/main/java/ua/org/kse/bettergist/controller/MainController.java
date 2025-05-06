f4b3e8776be239fe64bcf83089d465a22a41b342
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2025-05-06 10:58:20 +0300

    Basic endpoint

--- src/main/java/ua/org/kse/bettergist/controller/MainController.java
+++ src/main/java/ua/org/kse/bettergist/controller/MainController.java
@@ -1,9 +1,15 @@
 package ua.org.kse.bettergist.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+    @PostMapping
+    public Long createGist() {
+        // TODO
 
+        return 0L;
+    }
 }
